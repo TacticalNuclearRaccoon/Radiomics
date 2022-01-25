b07daee974b2a34ce6d2f70ea88735649b1b24cc
TacticalNuclearRaccoon <72969202+TacticalNuclearRaccoon@users.noreply.github.com>
2022-01-25 15:50:51 +0100

    Create functions.py

---
+++ Gliomas/functions.py
@@ -0,0 +1,258 @@
+import os
+import cv2
+import glob
+import PIL
+import shutil
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from skimage import data
+from skimage.util import montage 
+import skimage.transform as skTrans
+from skimage.transform import rotate
+from skimage.transform import resize
+from PIL import Image, ImageOps  
+
+# neural imaging
+import nilearn as nl
+import nibabel as nib
+import nilearn.plotting as nlplt
+!pip install git+https://github.com/miykael/gif_your_nifti # nifti to gif 
+import gif_your_nifti.core as gif2nif
+
+# machine learning
+import keras
+import keras.backend as K
+from keras.callbacks import CSVLogger
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+from tensorflow.keras.layers.experimental import preprocessing
+
+# dice loss for 4 classes: 0: background / 1: necrotic core / 2: edema / 3:enhancing tumor
+def dice_coef(y_true, y_pred, smooth=1.0):
+    class_num = 4
+    for i in range(class_num):
+        y_true_f = K.flatten(y_true[:,:,:,i])
+        y_pred_f = K.flatten(y_pred[:,:,:,i])
+        intersection = K.sum(y_true_f * y_pred_f)
+        loss = ((2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth))
+   #     K.print_tensor(loss, message='loss value for class {} : '.format(SEGMENT_CLASSES[i]))
+        if i == 0:
+            total_loss = loss
+        else:
+            total_loss = total_loss + loss
+    total_loss = total_loss / class_num
+#    K.print_tensor(total_loss, message=' total dice coef: ')
+    return total_loss
+
+# define per class evaluation of dice coef
+# inspired by https://github.com/keras-team/keras/issues/9395
+def dice_coef_necrotic(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,1] * y_pred[:,:,:,1]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,1])) + K.sum(K.square(y_pred[:,:,:,1])) + epsilon)
+
+def dice_coef_edema(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,2] * y_pred[:,:,:,2]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,2])) + K.sum(K.square(y_pred[:,:,:,2])) + epsilon)
+
+def dice_coef_enhancing(y_true, y_pred, epsilon=1e-6):
+    intersection = K.sum(K.abs(y_true[:,:,:,3] * y_pred[:,:,:,3]))
+    return (2. * intersection) / (K.sum(K.square(y_true[:,:,:,3])) + K.sum(K.square(y_pred[:,:,:,3])) + epsilon)
+
+# Computing Precision 
+def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    
+# Computing Sensitivity      
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+# Computing Specificity
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1-y_true) * (1-y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1-y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+  
+  #Defining the Unet model
+# source https://naomi-fridman.medium.com/multi-class-image-segmentation-a5cc671e647a
+
+def build_unet(inputs, ker_init, dropout):
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(inputs)
+    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv1)
+    
+    pool = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool)
+    conv = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+    
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv2)
+    
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv3)
+    
+    
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(pool4)
+    conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv5)
+    drop5 = Dropout(dropout)(conv5)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(drop5))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv9)
+    
+    up = Conv2D(32, 2, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(UpSampling2D(size = (2,2))(conv9))
+    merge = concatenate([conv1,up], axis = 3)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(merge)
+    conv = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = ker_init)(conv)
+    
+    conv10 = Conv2D(4, (1,1), activation = 'softmax')(conv)
+    
+    return Model(inputs = inputs, outputs = conv10)
+
+input_layer = Input((IMG_SIZE, IMG_SIZE, 2))
+
+model = build_unet(input_layer, 'he_normal', 0.2)
+model.compile(loss="categorical_crossentropy", optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), 
+              metrics = ['accuracy',tf.keras.metrics.MeanIoU(num_classes=4), 
+                         dice_coef, precision, sensitivity, specificity, dice_coef_necrotic, 
+                         dice_coef_edema ,dice_coef_enhancing] )
+
+#Loading data
+def pathListIntoIds(dirList):
+    x = []
+    for i in range(0,len(dirList)):
+        x.append(dirList[i][dirList[i].rfind('/')+1:])
+    return x
+
+train_and_test_ids = pathListIntoIds(train_and_val_directories); 
+
+    
+train_test_ids, val_ids = train_test_split(train_and_test_ids,test_size=0.2) 
+train_ids, test_ids = train_test_split(train_test_ids,test_size=0.15) 
+
+# show number of data for each dir 
+def showDataLayout():
+    plt.bar(["Train","Valid","Test"],
+    [len(train_ids), len(val_ids), len(test_ids)], align='center',color=[ 'green','red', 'blue'])
+    plt.legend()
+
+    plt.ylabel('Number of images')
+    plt.title('Data distribution')
+
+    plt.show()
+    
+showDataLayout()
+
+# mri type must one of the labels
+# returns volume of specified study at `path`
+def imageLoader(path):
+    image = nib.load(path).get_fdata()
+    X = np.zeros((self.batch_size*VOLUME_SLICES, *self.dim, self.n_channels))
+    for j in range(VOLUME_SLICES):
+        X[j +VOLUME_SLICES*c,:,:,0] = cv2.resize(image[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+        X[j +VOLUME_SLICES*c,:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE));
+
+        y[j +VOLUME_SLICES*c] = seg[:,:,j+VOLUME_START_AT];
+    return np.array(image)
+
+# Image Loader
+# load nifti file at `path`
+# and load each slice with mask from volume
+# choose the mri type & resize to `IMG_SIZE`
+def loadDataFromDir(path, list_of_files, mriType, n_images):
+    scans = []
+    masks = []
+    for i in list_of_files[:n_images]:
+        fullPath = glob.glob( i + '/*'+ mriType +'*')[0]
+        currentScanVolume = imageLoader(fullPath)
+        currentMaskVolume = imageLoader( glob.glob( i + '/*seg*')[0] ) 
+        # for each slice in 3D volume, find also it's mask
+        for j in range(0, currentScanVolume.shape[2]):
+            scan_img = cv2.resize(currentScanVolume[:,:,j], dsize=(IMG_SIZE,IMG_SIZE), interpolation=cv2.INTER_AREA).astype('uint8')
+            mask_img = cv2.resize(currentMaskVolume[:,:,j], dsize=(IMG_SIZE,IMG_SIZE), interpolation=cv2.INTER_AREA).astype('uint8')
+            scans.append(scan_img[..., np.newaxis])
+            masks.append(mask_img[..., np.newaxis])
+    return np.array(scans, dtype='float32'), np.array(masks, dtype='float32')
+
+# Predictions
+def predictByPath(case_path,case):
+    files = next(os.walk(case_path))[2]
+    X = np.empty((VOLUME_SLICES, IMG_SIZE, IMG_SIZE, 2))
+  #  y = np.empty((VOLUME_SLICES, IMG_SIZE, IMG_SIZE))
+    
+    vol_path = os.path.join(case_path, f'BraTS20_Training_{case}_flair.nii');
+    flair=nib.load(vol_path).get_fdata()
+    
+    vol_path = os.path.join(case_path, f'BraTS20_Training_{case}_t1ce.nii');
+    ce=nib.load(vol_path).get_fdata() 
+    
+ #   vol_path = os.path.join(case_path, f'BraTS20_Training_{case}_seg.nii');
+ #   seg=nib.load(vol_path).get_fdata()  
+
+    
+    for j in range(VOLUME_SLICES):
+        X[j,:,:,0] = cv2.resize(flair[:,:,j+VOLUME_START_AT], (IMG_SIZE,IMG_SIZE))
+        X[j,:,:,1] = cv2.resize(ce[:,:,j+VOLUME_START_AT], (IMG_SIZE,IMG_SIZE))
+ #       y[j,:,:] = cv2.resize(seg[:,:,j+VOLUME_START_AT], (IMG_SIZE,IMG_SIZE))
+        
+  #  model.evaluate(x=X,y=y[:,:,:,0], callbacks= callbacks)
+    return model.predict(X/np.max(X), verbose=1)
+
+
+def showPredictsById(case, start_slice = 60):
+    path = f"/content/BraTS2020_TrainingData/MICCAI_BraTS2020_TrainingData/BraTS20_Training_{case}"
+    gt = nib.load(os.path.join(path, f'BraTS20_Training_{case}_seg.nii')).get_fdata()
+    origImage = nib.load(os.path.join(path, f'BraTS20_Training_{case}_flair.nii')).get_fdata()
+    p = predictByPath(path,case)
+
+    core = p[:,:,:,1]
+    edema= p[:,:,:,2]
+    enhancing = p[:,:,:,3]
+
+    plt.figure(figsize=(18, 50))
+    f, axarr = plt.subplots(1,6, figsize = (18, 50)) 
+
+    for i in range(6): # for each image, add brain background
+        axarr[i].imshow(cv2.resize(origImage[:,:,start_slice+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE)), cmap="gray", interpolation='none')
+    
+    axarr[0].imshow(cv2.resize(origImage[:,:,start_slice+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE)), cmap="gray")
+    axarr[0].title.set_text('Original image flair')
+    curr_gt=cv2.resize(gt[:,:,start_slice+VOLUME_START_AT], (IMG_SIZE, IMG_SIZE), interpolation = cv2.INTER_NEAREST)
+    axarr[1].imshow(curr_gt, cmap="Reds", interpolation='none', alpha=0.3) # ,alpha=0.3,cmap='Reds'
+    axarr[1].title.set_text('Ground truth')
+    axarr[2].imshow(p[start_slice,:,:,1:4], cmap="Reds", interpolation='none', alpha=0.3)
+    axarr[2].title.set_text('all classes')
+    axarr[3].imshow(edema[start_slice,:,:], cmap="OrRd", interpolation='none', alpha=0.3)
+    axarr[3].title.set_text(f'{SEGMENT_CLASSES[1]} predicted')
+    axarr[4].imshow(core[start_slice,:,], cmap="OrRd", interpolation='none', alpha=0.3)
+    axarr[4].title.set_text(f'{SEGMENT_CLASSES[2]} predicted')
+    axarr[5].imshow(enhancing[start_slice,:,], cmap="OrRd", interpolation='none', alpha=0.3)
+    axarr[5].title.set_text(f'{SEGMENT_CLASSES[3]} predicted')
+    plt.show()
+    
